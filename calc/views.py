ce46aa8b27c238e6ddc94d9f7020f1832ee2555c
niharika <niharika.142308@gmail.com>
2021-05-29 00:53:37 +0530

    Added templates

--- calc/views.py
+++ calc/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(Request):
-   return HttpResponse("Hello World!")
+def home(request):
+   return render(request,'home.html')
